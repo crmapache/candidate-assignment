17748b8605427164f3f0de2ee4e614d84cb88233
Maksim Zolotoi <spacegoldy@gmail.com>
2021-12-01 15:47:42 -0600

    done

--- src/routes.ts
+++ src/routes.ts
@@ -5,31 +5,31 @@ import shoppingFlowRouteHandler from './shoppingFlowRouteHandler'
 
 export default new Router()
   .use(starterRoutes)
-
-  // Home page
   .match('/', shoppingFlowRouteHandler)
   .match('/home', shoppingFlowRouteHandler)
-
-  // PLP pages
   .match('/new/:path*', shoppingFlowRouteHandler)
 
-  // PDP pages
+  // Caching for the PDP pages of "bath-bombs" category
+  .match('/bath/bath-bombs/:path*', shoppingFlowRouteHandler)
 
+  // Caching for all static images served from the /dw/image/v2 path.
+  .match('/dw/image/v2/:path*', ({ cache, proxy }) => {
+    cache(CACHE_ASSETS)
+    return proxy('origin')
+  })
 
-
-  // example route for cacheable assets:
-  .match('/images/:path*', ({ cache, proxy }) => {
+  .match('/on/demandware.static/Sites-Lush-Site/:path*', ({ cache, proxy }) => {
     cache(CACHE_ASSETS)
     return proxy('origin')
   })
 
   .match('/service-worker.js', ({ serviceWorker }) => serviceWorker('dist/service-worker.js'))
+
   .match('/main.js', ({ serveStatic, cache }) => {
     cache(CACHE_ASSETS)
     return serveStatic('dist/browser.js')
   })
 
-  // fallback route for all other requests:
   .fallback(({ proxy }) => {
     proxy('origin')
   })
--- src/service-worker.ts
+++ src/service-worker.ts
@@ -5,6 +5,9 @@ import DeepFetchPlugin, { DeepFetchCallbackParam } from '@layer0/prefetch/sw/Dee
 skipWaiting()
 clientsClaim()
 
+// Prefetching for the PDP pages of "bath-bombs" category
+prefetch('/bath/bath-bombs/:path*')
+
 new Prefetcher({
   plugins: [
     new DeepFetchPlugin([
@@ -13,53 +16,36 @@ new Prefetcher({
         maxMatches: 1,
         attribute: 'src',
         as: 'image',
-        callback: deepFetchPLPImages,
+        callback: deepFetchImages,
       },
       {
         selector: '.plp-banner-top-inner img.plp-banner-top-image',
         maxMatches: 1,
         attribute: 'src',
         as: 'image',
-        callback: deepFetchPLPImagesHero,
+        callback: deepFetchImages,
       },
       {
         selector: 'img.product-tile-image.position-relative',
         maxMatches: 2,
         attribute: 'src',
         as: 'image',
-        callback: deepFetchPLPImagesTwo,
+        callback: deepFetchImages,
+      },
+      // Deep fetching for the main images for the PDP page of "bath-bombs" category.
+      {
+        selector: 'img.pdp-carousel-image',
+        maxMatches: 3,
+        attribute: 'src',
+        as: 'image',
+        callback: deepFetchImages,
       },
     ]),
   ],
 })
   .route()
-  // .cache(/^https:\/\/assets-global\.website-files\.com\/.*/)
-  // ENTER REGULAR EXPRESSION SELECTOR FOR IMAGES YOU WANT TO PREFETCH //
-  // (usualy as CDN base domain name followed by ".*" as general selecor) //
 
-///////////////////////////////////////////////
-// Callback function for PLP image selector //
-function deepFetchPLPImages({ $el, el, $ }: DeepFetchCallbackParam) {
-  const url = $el.attr('src')
-  console.log("[][]][][[][]][][][][][[]][[][][]\nPrefetching PLP Hero 1: "+url+"\n")
-  prefetch(url, 'image')
+// I created one function for all images because old functions was the same except logs.
+function deepFetchImages({ $el, el }: DeepFetchCallbackParam) {
+  prefetch($el.attr('src'), 'image')
 }
-
-function deepFetchPLPImagesHero({ $el, el, $ }: DeepFetchCallbackParam) {
-  const url = $el.attr('src')
-  console.log("[][]][][[][]][][][][][[]][[][][]\nPrefetching PLP Hero 2: "+url+"\n")
-  prefetch(url, 'image')
-}
-
-function deepFetchPLPImagesTwo({ $el, el, $ }: DeepFetchCallbackParam) {
-  const url = $el.attr('src')
-  console.log("[][]][][[][]][][][][][[]][[][][]\nPrefetching PLP: "+url+"\n")
-  prefetch(url, 'image')
-}
-
-
-
-// function logPrefetchedContent({$el}) { // for testing
-//   // console.log("[][]][][[][]][][][][][[]][[][][]")
-//   console.log("content '"+$el.attr('src')+"' has been prefetched...")
-// }
